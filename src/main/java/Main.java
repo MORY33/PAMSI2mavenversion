a17dc2f9484e6f337100d6a287b368f6a78a079d
MORY33 <rafa.karwot@gmail.com>
2022-05-03 20:29:16 +0200

    multithreads running from Sortit class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,64 @@
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
-        Movie tempList = new Movie();
-        tempList.CreateList();
-        Menu menu = new Menu();
-//        menu.menu();
-        tempList.printList(tempList.CreateList());
 
-        Runnable[] runners = new Runnable[3];
-        Thread[] threads = new Thread[3];
+        Sortit shortit = new Sortit(1);
+        shortit.runThreads();
 
+
+
+//        Movie tempList = new Movie();
+//        tempList.CreateList();
+//        Menu menu = new Menu();
+////        menu.menu();
+//        tempList.printList(tempList.CreateList());
+//
+//        Runnable[] runners = new Runnable[3];
+//        Thread[] threads = new Thread[3];
+//
+////        for (int i=0; i<3; i++){
+////            runners[i] = new Sortit(i);
+////        }
+//        runners[0] = new Sortit(tempList.CreateList(), Algorithm.QUICKSORT);
+//        runners[1] = new Sortit(tempList.CreateList(), Algorithm.MERGESORT);
+//        runners[2]= new Sortit(tempList.CreateList(), Algorithm.OTHERSORT);
+//
+//
+//
+////        Sortit tempQuick = new Sortit(tempList.CreateList(), Algorithm.QUICKSORT);
+////        Sortit tempMerge = new Sortit(tempList.CreateList(), Algorithm.QUICKSORT);
+////        Sortit tempOther = new Sortit(tempList.CreateList(), Algorithm.QUICKSORT);
+////
+////        tempQuick = (Sortit) runners[0];
+////        tempMerge = (Sortit) runners[1];
+////        tempOther = (Sortit) runners[2];
+////
+////        tempOther.quickSort();
+//
+//
+//        //tworzy i odpala wszystkie wątki
 //        for (int i=0; i<3; i++){
-//            runners[i] = new Sortit(i);
+//            threads[i] = new Thread(runners[i]);
 //        }
-        runners[0] = new Sortit(tempList.CreateList(), Algorithm.QUICKSORT);
-        runners[1] = new Sortit(tempList.CreateList(), Algorithm.MERGESORT);
-        runners[2]= new Sortit(tempList.CreateList(), Algorithm.OTHERSORT);
+//
+//        for (int i=0; i<3; i++){
+//            threads[i].start();
+//        }
 
-        //tworzy i odpala wszystkie wątki
-        for (int i=0; i<3; i++){
-            threads[i] = new Thread(runners[i]);
-        }
+//        Sortit shortItMerge = new Sortit(tempList.CreateList(), Algorithm.MERGESORT);
+//
+//        if (shortItMerge.mergeSort() == 0){
+//            threads[1].stop();
+//            System.out.println("Thread killed");
+//        }
 
-        for (int i=0; i<3; i++){
-            threads[i].start();
-        }
+
+
+
 
 
 
--- src/main/java/Sortit.java
+++ src/main/java/Sortit.java
@@ -1,8 +1,11 @@
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class Sortit implements Runnable{
 
     private int id;
+    private boolean exit = false;
     private Algorithm algorithm;
     private ArrayList<Movie> myMovies;
 
@@ -11,41 +14,119 @@ public class Sortit implements Runnable{
         this.algorithm = algorithm;
     }
 
+    ArrayList<Movie> tempList = new ArrayList<Movie>();
+
     public Sortit(int id){
         this.id = id;
     }
 
 
-    public void quickSort(){
+    public ArrayList<Movie> loadMovieList() throws IOException {
+        ArrayList<Movie> tempList = new ArrayList<Movie>();
+        Movie movieList = new Movie();
+        tempList = movieList.CreateList();
+        return tempList;
+    }
+
+
+
+    public void runThreads() throws IOException {
+
+        Movie tempList = new Movie();
+        tempList.CreateList();
+        tempList.printList(tempList.CreateList());
+
+        Runnable[] runners = new Runnable[3];
+        Thread[] threads = new Thread[3];
+
+        runners[0] = new Sortit(tempList.CreateList(), Algorithm.QUICKSORT);
+        runners[1] = new Sortit(tempList.CreateList(), Algorithm.MERGESORT);
+        runners[2]= new Sortit(tempList.CreateList(), Algorithm.OTHERSORT);
+
+        //tworzy i odpala wszystkie wątki
+        for (int i=0; i<3; i++){
+            threads[i] = new Thread(runners[i]);
+        }
+
+        for (int i=0; i<3; i++){
+            threads[i].start();
+        }
+
+    }
+
+
+    public ArrayList<Movie> quickSort() throws InterruptedException {
+        ArrayList<Movie> tempmovie = new ArrayList<Movie>();
         System.out.println("quick sorting...");
+
+
+
+//        TimeUnit.SECONDS.sleep(2);
+//        stop();
+        return tempmovie;
     }
 
-    public void mergeSort(){
+
+
+    public int mergeSort() throws InterruptedException {
         System.out.println("merge sorting...");
+        int i = 5;
+
+        TimeUnit.SECONDS.sleep(2);
+        i = 0;
+//        stop();
+        return i;
     }
 
-    public void otherSort(){
+    public void otherSort() throws InterruptedException {
         System.out.println("other sorting...");
+
+
+
+//        TimeUnit.SECONDS.sleep(12);
+//        stop();
+    }
+
+
+    public void stop()
+    {
+       exit = true;
     }
 
     @Override
     public void run() {
-        while(true){
+
+        while(!exit){
+
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
             if(algorithm.equals(Algorithm.QUICKSORT)){
-                quickSort();
-           }
+                try {
+                    quickSort();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
            else if(algorithm.equals(Algorithm.MERGESORT)){
-               mergeSort();
-           }
+                try {
+                    mergeSort();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
            else if (algorithm.equals(Algorithm.OTHERSORT)){
-               otherSort();
-           }
+                try {
+                    otherSort();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
         }
+
 
     }
 
